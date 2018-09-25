61e439b07d273858dafd6f6c9dec6af0eb4081d1
takehilo <takehilo.kaneko@gmail.com>
2018-09-25 12:05:54 +0900

    Initial Commit

---
+++ FluxPatternExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FluxPatternExample
+//
+//  Created by Takehiro Kaneko on 2018/09/25.
+//  Copyright © 2018年 Takehiro Kaneko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
